ecad3dee06ddff9465c48ff57b265b49e538d2da
Hsufeng Lee <thittalee@gmail.com>
2021-06-25 01:07:46 +0800

    add gh-pages

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          你是一隻噗噗噗的豬
         </p>
         <a
           className="App-link"
